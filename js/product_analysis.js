92866d00f8d43ff1f1e85266cccdf70248c0bba7
stanJ <cjiao@3ti.us>
2017-06-21 23:49:10 +0800

    快24点了

--- js/product_analysis.js
+++ js/product_analysis.js
@@ -19,57 +19,174 @@ var app = new Vue({
 		},
 		loadCategoryList: function(){
 			this.categoryData = [{
-				'1': 'xxx',
-				'2': 'xxx',
-				'3': 'xxx',
+				'1': 'Apple iPhone 6s Plus',
+				'2': '32G 玫瑰金色 移动联通电信4G手机',
+				'3': '￥4099.00',
 			},{
-				'1': 'xxx',
-				'2': 'xxx',
-				'3': 'xxx',
+				'1': 'Apple iPhone 7',
+				'2': '128G 红色特别版 移动联通电信4G手机',
+				'3': '￥5699.00',
 			},{
-				'1': 'xxx',
-				'2': 'xxx',
-				'3': 'xxx',
+				'1': 'Apple iPhone 6s',
+				'2': '32G 金色 移动联通电信4G手机',
+				'3': '￥3999.00',
 			},]
 		},
 		loadHighlightList: function(){
-			this.highlightData = [{
+			this.highlightData = [
+			{
 				'0': '双卡双待',
-				'1': 'xxx',
-				'2': 'xxx',
-				'3': 'xxx',
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
 			},{
 				'0': '指纹识别',
-				'1': 'xxx',
-				'2': 'xxx',
-				'3': 'xxx',
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
 			},{
 				'0': '拍照神器',
-				'1': 'xxx',
-				'2': 'xxx',
-				'3': 'xxx',
-			},]
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},
+			{
+				'0': '金属机身',
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},{
+				'0': '快速充电',
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},{
+				'0': '骁龙芯片',
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},
+			{
+				'0': '以旧换新',
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},{
+				'0': '女性手机',
+				'1': 'true',
+				'2': 'false',
+				'3': 'true',
+				'4': 'false',
+				'5': 'false',
+			},{
+				'0': '支持NFC',
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},
+			{
+				'0': '直板键盘',
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},{
+				'0': '后置摄像头',
+				'1': 'true',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},{
+				'0': '拍照神器',
+				'1': 'true',
+				'2': 'false',
+				'3': 'true',
+				'4': 'false',
+				'5': 'false',
+			},
+			{
+				'0': '双卡双待',
+				'1': 'false',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},{
+				'0': '指纹识别',
+				'1': 'false',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},{
+				'0': '拍照神器',
+				'1': 'false',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},{
+				'0': '指纹识别',
+				'1': 'false',
+				'2': 'false',
+				'3': 'false',
+				'4': 'false',
+				'5': 'false',
+			},
+			]
 		},
 		loadSalesList: function(){
-			this.salesData = [{
-				'1': 'xxx',
-				'2': 'xxx',
-				'3': 'xxx',
-				'4': 'xxx',
-				'5': 'xxx',
+			this.salesData = [
+			{
+				'1': 'MIUI/小米',
+				'2': 'true',
+				'3': '1222',
+				'4': '3311',
+				'5': '4333',
 			},{
-				'1': 'xxx',
-				'2': 'xxx',
-				'3': 'xxx',
-				'4': 'xxx',
-				'5': 'xxx',
+				'1': 'Apple/苹果',
+				'2': 'true',
+				'3': '1222',
+				'4': '3311',
+				'5': '4333',
 			},{
-				'1': 'xxx',
-				'2': 'xxx',
-				'3': 'xxx',
-				'4': 'xxx',
-				'5': 'xxx',
-			},]
+				'1': 'Huawei/华为',
+				'2': 'true',
+				'3': '1222',
+				'4': '3311',
+				'5': '4333',
+			},{
+				'1': 'Samsung/三星',
+				'2': 'true',
+				'3': '1222',
+				'4': '3311',
+				'5': '4333',
+			},{
+				'1': 'VIVO',
+				'2': 'true',
+				'3': '1222',
+				'4': '3311',
+				'5': '4333',
+			},
+			]
 		},
 		loadCommentActiveList: function(){
 			this.commentActiveData = [{
@@ -103,4 +220,81 @@ var app = new Vue({
 		},
 	}
 })
-			
+$(function(){
+	lineStay();
+})
+function lineStay(){
+	var line_stay = echarts.init($(".chart--line--stay")[0]);
+	var option = {
+		grid: {
+			left: '35',
+			right: '10',
+			top: '26',
+			bottom: '20'
+		},
+        tooltip: {},
+        legend: {
+            data:[{
+            	name: '好评分数',
+            	icon: 'circle',
+            }],
+            right: '0',
+            top: '0',
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: true,
+            data:['1月','2月','3月','4月','5月',],
+            axisLine: {
+	            lineStyle: {
+	        		color: '#d7d7d7',
+	        		width: 2,
+	        	}
+	        },
+	        axisTick: {
+	        	show: false,
+	        }
+        },
+        yAxis: {
+        	axisLine: {
+	            show: false,
+	        },
+	        splitLine: {
+	        	lineStyle: {
+	        		color: '#f7f9fa',
+	        	}
+	        },
+	        axisTick: {
+	        	show: false,
+	        },
+	        max: 100,
+        },
+        series: [{
+            name: '好评分数',
+            type: 'line',
+            lineStyle: {
+            	normal: {
+            		color: '#51a5de'
+            	}
+            	
+            },
+            itemStyle: {
+            	normal: {
+            		color: '#51a5de'
+            	}
+            	
+            },
+            'symbol': 'emptyCircle',
+            symbolSize: 8,
+            hoverAnimation: false,
+            data: [
+            	['1月', 10],
+            	['2月', 30],
+            	['3月', 40],
+            	['4月', 20],
+            	['5月', 80],
+            ]
+        }]
+    };
+    line_stay.setOption(option)
+}
